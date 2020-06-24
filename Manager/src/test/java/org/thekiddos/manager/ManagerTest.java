a895d198bd2275f116532c2271714ffc29e56840
J2mF <mp4-12cs5@hotmail.com>
2020-06-24 14:34:53 +0300

    Refactored tests
    Change Log:
    Refactored ManagerTest and PayrollTest

--- Manager/src/test/java/org/thekiddos/manager/ManagerTest.java
+++ Manager/src/test/java/org/thekiddos/manager/ManagerTest.java
@@ -14,23 +14,22 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ManagerTest {
+    private Long tableId = 1L, customerId = 1L, itemId = 1L;
 
     @BeforeEach
     void setUpDatabase() {
         Database.init();
+        fillDatabase();
+    }
+
+    private void fillDatabase() {
+        new AddTableTransaction( tableId ).execute();
+        new AddCustomerTransaction( customerId, "Kiddo", "Zahlt" ).execute();
+        new AddItemTransaction( itemId, "French Fries", 10.0 ).execute();
     }
 
     @Test
     void testPayForOrder() {
-        Long tableId = 20L;
-        new AddTableTransaction( tableId ).execute();
-
-        Long customerId = 20L;
-        new AddCustomerTransaction( customerId, "Kiddo", "Zahlt" ).execute();
-
-        Long itemId = 20L;
-        new AddItemTransaction( itemId, "French Fries", 10.0 ).execute();
-
         new ImmediateReservationTransaction( tableId, customerId ).execute();
 
         AddServiceTransaction service = new AddReservationServiceTransaction( tableId );
@@ -65,15 +64,6 @@ public class ManagerTest {
 
     @Test
     void testPayForOrderWithInActiveReservation() {
-        Long tableId = 20L;
-        new AddTableTransaction( tableId ).execute();
-
-        Long customerId = 20L;
-        new AddCustomerTransaction( customerId, "Kiddo", "Zahlt" ).execute();
-
-        Long itemId = 20L;
-        new AddItemTransaction( itemId, "French Fries", 10.0 ).execute();
-
         LocalDate fifthOfNovember2020 = LocalDate.of( 2020, Month.NOVEMBER, 5 );
         LocalTime eightPM = LocalTime.of( 20, 0 );
         new ScheduledReservationTransaction( tableId, customerId, fifthOfNovember2020, eightPM ).execute();
@@ -89,14 +79,7 @@ public class ManagerTest {
         assertEquals( 1, tableReservations.size() );
     }
 
-    // TODO use package protection for the setters
-    // TODO maybe Types should be a class or a string for dynamic use
-    // When deleting something move it to another table instead
-    // can we replace Order with a list?
     // Database should be transparent to all classes
-    // paying orders should delete/move the reservation
+    // paying orders should move the reservation
     // test two reservation and add orders to them and pay
-    // remember to save the reservation object after retrieving from an actual database
-    // save the invoice in the database
-    // TODO again Make sure we can't mutate anything outside of transactions(package)
 }
--- Manager/src/test/java/org/thekiddos/manager/PayrollTest.java
+++ Manager/src/test/java/org/thekiddos/manager/PayrollTest.java
@@ -12,18 +12,21 @@ import java.time.Month;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PayrollTest {
+    private Long empId = 1L, empId2 = 2L;
 
     @BeforeEach
     void setUpDatabase() {
         Database.init();
+        fillDatabase();
+    }
+
+    private void fillDatabase() {
+        new AddSalariedEmployeeTransaction( empId, "Zahlt", 1000.0 ).execute();
+        new AddHourlyEmployeeTransaction( empId2, "Zahlt", 8.0 ).execute();
     }
 
     @Test
     void testPaySingleSalariedEmployee() {
-        Long empId = 1L;
-        Transaction addEmployee = new AddSalariedEmployeeTransaction( empId, "Zahlt", 1000.0 );
-        addEmployee.execute();
-
         LocalDate endOfJune2020 = LocalDate.of( 2020, Month.JUNE, 30 );
         PayDayTransaction payDay = new PayDayTransaction( endOfJune2020 );
         payDay.execute();
@@ -37,10 +40,6 @@ class PayrollTest {
     }
 
     @Test void testPayingSingleSalariedEmployeeOnWrongDate() {
-        Long empId = 1L;
-        Transaction addEmployee = new AddSalariedEmployeeTransaction( empId, "Zahlt", 1000.0 );
-        addEmployee.execute();
-
         LocalDate june29Year2020 = LocalDate.of( 2020, Month.JUNE, 29 );
         PayDayTransaction payDay = new PayDayTransaction( june29Year2020 );
         payDay.execute();
@@ -51,110 +50,86 @@ class PayrollTest {
 
     @Test
     void testPaySingleHourlyEmployeeNoTimeCards() {
-        Long empId = 1L;
-        Transaction addEmployee = new AddHourlyEmployeeTransaction( empId, "Zahlt", 8.0 );
-        addEmployee.execute();
-
         LocalDate payDate = LocalDate.of( 2020, Month.JUNE, 13 ); // Saturday
         PayDayTransaction payDay = new PayDayTransaction( payDate );
         payDay.execute();
 
-        validateHourlyPaycheck( payDay, empId, payDate, 0.0 );
+        validateHourlyPaycheck( payDay, empId2, payDate, 0.0 );
     }
 
     @Test
     void testPaySingleHourlyEmployeeOneTimeCard() {
-        Long empId = 1L;
-        Transaction addEmployee = new AddHourlyEmployeeTransaction( empId, "Zahlt", 8.0 );
-        addEmployee.execute();
-
         LocalDate payDate = LocalDate.of( 2020, Month.JUNE, 13 ); // Saturday
         LocalTime threeHoursAndThirtyMinutes = LocalTime.of( 3, 30 );
 
-        Transaction addTimeCard = new AddTimeCardTransaction( empId, payDate, threeHoursAndThirtyMinutes );
+        Transaction addTimeCard = new AddTimeCardTransaction( empId2, payDate, threeHoursAndThirtyMinutes );
         PayDayTransaction payDay = new PayDayTransaction( payDate );
 
         addTimeCard.execute();
         payDay.execute();
 
-        validateHourlyPaycheck( payDay, empId, payDate, 28.0 );
+        validateHourlyPaycheck( payDay, empId2, payDate, 28.0 );
     }
 
     @Test
     void testPaySingleHourlyEmployeeOverTimeOneTimeCard() {
-        Long empId = 1L;
-        Transaction addEmployee = new AddHourlyEmployeeTransaction( empId, "Zahlt", 8.0 );
-        addEmployee.execute();
-
         LocalDate payDate = LocalDate.of( 2020, Month.JUNE, 13 ); // Saturday
         LocalTime nineHours = LocalTime.of( 9, 0 );
 
-        Transaction addTimeCard = new AddTimeCardTransaction( empId, payDate, nineHours );
+        Transaction addTimeCard = new AddTimeCardTransaction( empId2, payDate, nineHours );
         PayDayTransaction payDay = new PayDayTransaction( payDate );
 
         addTimeCard.execute();
         payDay.execute();
 
         // 9 hours of work means 8 hours are paid normally while the extra hour is paid 1.5 times the normal hour
-        validateHourlyPaycheck( payDay, empId, payDate, ( 8.0 + 1.5 ) * 8.0 );
+        validateHourlyPaycheck( payDay, empId2, payDate, ( 8.0 + 1.5 ) * 8.0 );
     }
 
     @Test
     void testPaySingleHourlyEmployeeOnWrongDate() {
-        Long empId = 1L;
-        Transaction addEmployee = new AddHourlyEmployeeTransaction( empId, "Zahlt", 8.0 );
-        addEmployee.execute();
-
         LocalDate payDate = LocalDate.of( 2020, Month.JUNE, 12 ); // Friday
         LocalTime threeHoursAndThirtyMinutes = LocalTime.of( 3, 30 );
 
-        Transaction addTimeCard = new AddTimeCardTransaction( empId, payDate, threeHoursAndThirtyMinutes );
+        Transaction addTimeCard = new AddTimeCardTransaction( empId2, payDate, threeHoursAndThirtyMinutes );
         PayDayTransaction payDay = new PayDayTransaction( payDate );
 
         addTimeCard.execute();
         payDay.execute();
 
-        PayCheck payCheck = payDay.getPayCheck( empId );
+        PayCheck payCheck = payDay.getPayCheck( empId2 );
         assertNull( payCheck );
     }
 
     @Test
     void testPaySingleHourlyEmployeeTwoTimeCard() {
-        Long empId = 1L;
-        Transaction addEmployee = new AddHourlyEmployeeTransaction( empId, "Zahlt", 8.0 );
-        addEmployee.execute();
-
         LocalDate beforePayDate = LocalDate.of( 2020, Month.JUNE, 12 ); // Friday
         LocalDate payDate = LocalDate.of( 2020, Month.JUNE, 13 ); // Saturday
         LocalTime threeHoursAndThirtyMinutes = LocalTime.of( 3, 30 );
         LocalTime fourHours = LocalTime.of( 4, 0 );
 
-        Transaction addTimeCard = new AddTimeCardTransaction( empId, beforePayDate, threeHoursAndThirtyMinutes );
-        Transaction addTimeCard2 = new AddTimeCardTransaction( empId, payDate, fourHours );
+        Transaction addTimeCard = new AddTimeCardTransaction( empId2, beforePayDate, threeHoursAndThirtyMinutes );
+        Transaction addTimeCard2 = new AddTimeCardTransaction( empId2, payDate, fourHours );
         PayDayTransaction payDay = new PayDayTransaction( payDate );
 
         addTimeCard.execute();
         addTimeCard2.execute();
         payDay.execute();
 
-        validateHourlyPaycheck( payDay, empId, payDate, 60.0 );
+        validateHourlyPaycheck( payDay, empId2, payDate, 60.0 );
     }
 
     @Test
     void testPaySingleHourlyEmployeeWithTimeCardsSpanningThreePayPeriods() {
-        Long empId = 1L;
-        Transaction addEmployee = new AddHourlyEmployeeTransaction( empId, "Zahlt", 8.0 );
-        addEmployee.execute();
-
         LocalDate afterPayDate = LocalDate.of( 2020, Month.JUNE, 14 ); // Sunday
         LocalDate payDate = LocalDate.of( 2020, Month.JUNE, 13 ); // Saturday
         LocalDate previousPayDate = LocalDate.of( 2020, Month.JUNE, 6 ); // Previous Saturday
         LocalTime threeHoursAndThirtyMinutes = LocalTime.of( 3, 30 );
         LocalTime fourHours = LocalTime.of( 4, 0 );
 
-        Transaction addTimeCard = new AddTimeCardTransaction( empId, payDate, threeHoursAndThirtyMinutes ); // the only valid one
-        Transaction addTimeCard2 = new AddTimeCardTransaction( empId, afterPayDate, fourHours );
-        Transaction addTimeCard3 = new AddTimeCardTransaction( empId, previousPayDate, fourHours );
+        Transaction addTimeCard = new AddTimeCardTransaction( empId2, payDate, threeHoursAndThirtyMinutes ); // the only valid one
+        Transaction addTimeCard2 = new AddTimeCardTransaction( empId2, afterPayDate, fourHours );
+        Transaction addTimeCard3 = new AddTimeCardTransaction( empId2, previousPayDate, fourHours );
         PayDayTransaction payDay = new PayDayTransaction( payDate );
 
         addTimeCard.execute();
@@ -162,7 +137,7 @@ class PayrollTest {
         addTimeCard3.execute();
         payDay.execute();
 
-        validateHourlyPaycheck( payDay, empId, payDate, 28.0 );
+        validateHourlyPaycheck( payDay, empId2, payDate, 28.0 );
     }
 
     private void validateHourlyPaycheck( PayDayTransaction payDay, Long empId, LocalDate payDate, double expectedAmount ) {
