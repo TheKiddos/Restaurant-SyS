53e90e0c7384ed99695c80f8b3354619c01a3c6d
J2mF <mp4-12cs5@hotmail.com>
2020-07-20 12:48:22 +0300

    Added flutter waiter app module

---
+++ waiter/android/app/src/main/java/org/thekiddos/app/waiter/waiter/MainActivity.java
@@ -0,0 +1,13 @@
+package org.thekiddos.app.waiter.waiter;
+
+import android.support.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
