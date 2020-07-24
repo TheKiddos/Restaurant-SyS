0b3e8f429ba63d98df76facf2da1a6968e043020
J2mF <mp4-12cs5@hotmail.com>
2020-07-24 20:04:24 +0300

    [CHECKPOINT] Implementing Waiter App
    Main Changes to App:
    Renamed Module
    renamed dart files to lower cases
    fixed type.dart from different files
    app now access Item images through the website
    small refactoring
    Added App Icon

---
+++ Waiter/lib/menu.dart
@@ -0,0 +1,507 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'const.dart';
+import 'item.dart';
+import 'send.dart';
+import 'type.dart';
+
+class Menu extends StatefulWidget {
+  final int _tableId;
+  final List<Item> _items;
+  final List<Type> _types;
+
+  Menu( this._tableId, this._items, this._types );
+  @override
+  _MenuState createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  Widget build(BuildContext context) {
+    var sizeWidth = MediaQuery.of(context).size.width;
+    //  List_photo for  Page view
+    List <Widget> lis1=new List<Widget>();
+    List <Widget> lis2=new List<Widget>();
+    List <Widget> lis4=new List<Widget>();
+    List <Widget> lis5=new List<Widget>();
+
+    void f( var img,var liss){
+      for(int i=0;i<img.length;i++){
+        var imgg=Padding(
+          padding: EdgeInsets.fromLTRB(5, 2, 15, 10),
+          child: Container(
+            child: Stack(
+              children: <Widget>[
+                ClipRRect(
+                    borderRadius: BorderRadius.all(
+                        Radius.circular(50.0)),
+                    child:
+                    Image.asset(
+                      img[i],fit:BoxFit.fitWidth,height: 150,
+                    )
+                ),
+              ],
+            ),
+          ),
+        );
+        liss.add(imgg);
+      }
+
+
+    }
+    f(img1,lis1);
+    f(img2,lis2);
+    f(img4,lis4);
+    f(img5,lis5);
+    //////////////////////////////////////////////////////////
+    Tab tab(var t) {
+      return Tab(
+        text: t,
+      );
+    }
+//    PageController controller=PageController(viewportFraction: 0.8,initialPage: 1);
+    return
+      DefaultTabController(
+        length: 4,
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.black,
+              centerTitle: true,
+              title: Text('Menu',style: ktextstyle,),
+              actions: <Widget>[
+                RawMaterialButton(
+                  constraints:BoxConstraints.tightFor(
+                    width: 46,
+                  ),
+                  shape: CircleBorder(),
+                  fillColor: Colors.white12,
+                  child: Text("Next",style: etextstyle,),
+                  onPressed: (){
+                    Navigator.push(context,
+                        MaterialPageRoute(builder:(context) =>  Send()));
+                  },
+                )
+              ],
+              bottom: TabBar(
+                labelColor: Colors.white,
+                labelPadding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                tabs: <Widget>[
+                  tab('Main Meal'),tab('Drinks'),tab('Dessert'),tab('Appetizers')
+                ],
+              ),
+            ),
+            drawer: Drawer(
+              child: Text('LOL'),
+            ),
+            backgroundColor: Colors.white12,
+            body: TabBarView(
+              children: <Widget>[
+                //////////////////////////////////////////////
+                ListView(
+                  children: <Widget>[
+                    PageView(lis: lis1),
+                    Column(
+                      children: <Widget>[
+                        Image.asset('images/c.png',
+                          fit: BoxFit.fitWidth,
+                          width: sizeWidth,
+                          height: 200,
+                        ),
+                        SizedBox(height: 10,),
+                        RealMenu(
+                          sizeBox: sizeWidth ,
+                          im: Image.asset(
+                            'images/Pizza.png',
+                            fit: BoxFit.cover,
+                            width: 100,
+                            height: 150,
+                          ),
+                          t1: Text('Pizza ',style: etextstyle,),
+                          t2: Text('price is 50',style: etextstyle),
+
+                        ),
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth , im:
+                        Image.asset(
+                          'images/Burgers.png',
+                          fit: BoxFit.cover,
+                          width: 100,
+                          height: 150,
+                        ),
+                          t1: Text('Burger',style: etextstyle,),
+                          t2: Text('price is 50',style: etextstyle),
+                        ),
+                        SizedBox(height: 10,),
+                        Image.asset('images/back.jpg',
+                          fit: BoxFit.fitWidth,
+                          width: sizeWidth,
+                          height: 200,
+                        ),
+                        RealMenu(sizeBox: sizeWidth , im:
+                        Image.asset(
+                          'images/Sushi.png',
+                          fit: BoxFit.cover,
+                          width: 100,
+                          height: 150,
+                        ),
+                          t1: Text('Sushi',style: etextstyle),
+                          t2: Text('price is 50',style: etextstyle),
+                        ),
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth , im:
+                        Image.asset(
+                          'images/Spaghetti.png',
+                          fit: BoxFit.cover,
+                          width: 100,
+                          height: 150,
+                        ),
+                          t1: Text('Spaghetti',style: etextstyle),
+                          t2: Text('price is 50',style: etextstyle),
+
+                        ),
+                        Image.asset('images/bb.png',
+                          fit: BoxFit.fitWidth,
+                          width: sizeWidth,
+                          height: 200,
+                        ),
+
+                      ],
+                    )
+                  ],
+                ),
+                //////////////////////////////////////////////
+                ListView(
+                  children: <Widget>[
+                    PageView(lis: lis2),
+                    Column(
+                      children: <Widget>[
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth , im:
+                        Image.asset(
+                          'images/drink1.jpg',
+                          fit: BoxFit.cover,
+                          width: 100,
+                          height: 150,
+                        ),
+                          t1: Text('assaassasa'),
+                          t2: Text('assaassasa'),
+                        ),
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth , im:
+                        Image.asset(
+                          'images/drink2.png',
+                          fit: BoxFit.cover,
+                          width: 100,
+                          height: 150,
+                        ),
+                          t1: Text('assaassasa'),
+                          t2: Text('assaassasa'),
+                        ),
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth , im:
+                        Image.asset(
+                          'images/hot_chocolate.png',
+                          fit: BoxFit.cover,
+                          width: 100,
+                          height: 150,
+                        ),
+                          t1: Text('assaassasa'),
+                          t2: Text('assaassasa'),
+                        ),
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth , im:
+                        Image.asset(
+                          'images/milkshake.jpg',
+                          fit: BoxFit.cover,
+                          width: 100,
+                          height: 150,
+                        ),
+                          t1: Text('assaassasa'),
+                          t2: Text('assaassasa'),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                //////////////////////////////////////////////
+                ListView(
+                  children: <Widget>[
+                    PageView(lis: lis4),
+                    Column(
+                      children: <Widget>[
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth ,
+                          im: Image.asset(
+                            'images/des1.jpg',
+                            fit: BoxFit.cover,
+                            width: 100,
+                            height: 150,
+                          ),
+                          t1: Text('assaassasa'),
+                          t2: Text('assaassasa'),
+                        ),
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth ,
+                          im: Image.asset(
+                            'images/pan1.jpg',
+                            fit: BoxFit.cover,
+                            width: 100,
+                            height: 150,
+                          ),
+                          t1: Text('assaassasa'),
+                          t2: Text('assaassasa'),
+                        ),
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth ,
+                          im: Image.asset(
+                            'images/cheescake.png',
+                            fit: BoxFit.cover,
+                            width: 100,
+                            height: 150,
+                          ),
+                          t1: Text('assaassasa'),
+                          t2: Text('assaassasa'),
+                        ),
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth ,
+                          im: Image.asset(
+                            'images/knaf.png',
+                            fit: BoxFit.cover,
+                            width: 100,
+                            height: 150,
+                          ),
+                          t1: Text('assaassasa'),
+                          t2: Text('assaassasa'),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                //////////////////////////////////////////////
+                ListView(
+                  children: <Widget>[
+                    PageView(lis: lis1),
+                    Column(
+                      children: <Widget>[
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth ,
+                          im: Image.asset(
+                            'images/des1.jpg',
+                            fit: BoxFit.cover,
+                            width: 100,
+                            height: 150,
+                          ),
+                          t1: Text('assaassasa'),
+                          t2: Text('assaassasa'),
+                        ),
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth ,
+                          im: Image.asset(
+                            'images/des1.jpg',
+                            fit: BoxFit.cover,
+                            width: 100,
+                            height: 150,
+                          ),
+                          t1: Text('assaassasa'),
+                          t2: Text('assaassasa'),
+                        ),
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth ,
+                          im: Image.asset(
+                            'images/des1.jpg',
+                            fit: BoxFit.cover,
+                            width: 100,
+                            height: 150,
+                          ),
+                          t1: Text('assaassasa'),
+                          t2: Text('assaassasa'),
+                        ),
+                        SizedBox(height: 10,),
+                        RealMenu(sizeBox: sizeWidth ,
+                          im: Image.asset(
+                            'images/des1.jpg',
+                            fit: BoxFit.cover,
+                            width: 100,
+                            height: 150,
+                          ),
+                          t1: Text('assaassasa'),
+                          t2: Text('assaassasa'),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                //////////////////////////////////////////////
+              ],
+            )
+        ),
+      );
+  }
+}
+
+class Button extends StatelessWidget {
+  const Button({@required this.icon, @required  this.onPressed}) ;
+  final IconData icon;
+  final Function onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      elevation: 0.0,
+      child: Icon(icon),
+      onPressed: onPressed,
+      constraints:BoxConstraints.tightFor(
+          width: 56,
+          height: 56
+      ),
+      shape: CircleBorder(),
+      fillColor: Colors.white54,
+    );
+  }
+}
+// ignore: must_be_immutable
+class RealMenu extends StatefulWidget {
+  RealMenu({@required this.sizeBox,this.im,this.t1,this.t2,}) ;
+  final double sizeBox;
+  final Widget im;
+  final Widget t1;
+  final Widget t2;
+  @override
+  _RealMenuState createState() => _RealMenuState();
+}
+class _RealMenuState extends State<RealMenu> {
+  int quantity = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Cont(
+      size: widget.sizeBox,
+      single: Single(
+          ro: Roro(
+            imge: widget.im,
+            tex1: widget.t1,
+            tex2: widget.t2,
+            tex3: Text(quantity.toString(),style: ktextstyle,),
+            but1: Button(
+              icon: Icons.remove,
+              onPressed: (){
+                setState(() {
+                  if(quantity>0)
+                    --quantity;
+                });
+              },
+            ),
+            but2: Button(
+              icon: Icons.add,
+              onPressed: (){
+                setState(() {
+                  ++this.quantity;
+                });
+              },
+            ),
+
+
+
+          )
+      ),
+    );
+  }
+}
+class Roro extends StatelessWidget {
+  const Roro({@required  this.imge ,this.tex1,this.tex2,this.but1,this.but2,this.tex3}) ;
+  final imge,tex1,tex2,tex3,but1,but2;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        imge,
+        Container(
+          margin: EdgeInsets.fromLTRB(2, 0, 10, 0),
+          padding: EdgeInsets.fromLTRB(2, 0, 20, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              tex1,
+              Container(
+                padding: EdgeInsets.only(top: 20),
+                child: tex2,
+              )
+
+            ],
+          ),
+        ),
+        Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              tex3,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  but1,
+                  Padding(
+                      padding:  EdgeInsets.only(left: 45),
+                      child: but2
+                  )
+
+                ],
+              ),
+            ],
+          ),
+        )
+
+
+
+
+      ],
+    );
+  }
+}
+class Single extends StatelessWidget {
+  const Single({@required this.ro}) ;
+  final Widget ro;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView
+      (
+      scrollDirection: Axis.horizontal,
+      child: ro,
+    );
+  }
+}
+class Cont extends StatelessWidget {
+  const Cont({ @required this.size,this.single}) ;
+
+  final double size;
+  final Widget single;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white10,
+      width:size ,
+      height: 150,
+      margin:EdgeInsets.all(10),
+      child: single,
+    );
+  }
+}
+class PageView extends StatelessWidget {
+  const PageView({ @required this.lis,}) ;
+
+  final List<Widget> lis;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: lis,
+      ),
+    );
+  }
+}
---
+++ Waiter/lib/welcome_page.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+import 'const.dart';
+import 'item.dart';
+import 'manager_api.dart';
+import 'menu.dart';
+import 'type.dart';
+
+class WelcomeMenu extends StatefulWidget {
+  @override
+  _WelcomeMenuState createState() => _WelcomeMenuState();
+}
+
+class _WelcomeMenuState extends State<WelcomeMenu> {
+  final _formKey = GlobalKey<FormState>();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //AppBar section
+        appBar: AppBar(
+          backgroundColor: Colors.black12,
+          centerTitle: true,
+          title: Text(
+            'Digital Restaurant System™',
+            style: TextStyle(fontSize: 28.0),
+          ),
+        ),
+        backgroundColor: Colors.white12,
+        //Body section
+        body: buildMainBody());
+  }
+
+  Widget buildMainBody() {
+    return SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: ListView(
+                  children: <Widget>[
+                    welcomeImage,
+                    welcomeMessage,
+                  ],
+                ),
+              ),
+              TableIdForm(formKey: _formKey)
+            ],
+      ));
+  }
+
+  final Image welcomeImage = Image.asset('images/im1.png', height: 300 ,fit: BoxFit.fitWidth,);
+
+  final welcomeMessage = Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Text(
+      'Welcome to the restaurant'
+          ' please put your table number',
+      style: ktextstyle,
+    ),
+  );
+}
+
+class TableIdForm extends StatelessWidget {
+  const TableIdForm({
+    @required GlobalKey<FormState> formKey,
+  })  : _formKey = formKey;
+
+  final GlobalKey<FormState> _formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          //TextFormField
+          TableIdField(),
+          //Button section
+          GoToMenuButton(formKey: _formKey),
+        ],
+      ),
+    );
+  }
+}
+
+class TableIdField extends StatefulWidget {
+  @override
+  _TableIdFieldState createState() => _TableIdFieldState();
+}
+
+int tableId;
+class _TableIdFieldState extends State<TableIdField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 10),
+      child: TextFormField(
+        showCursor: true,
+        cursorColor: Colors.white,
+        maxLength: 5,
+        keyboardType: TextInputType.number,
+        validator: validateAndSetTableId,
+        decoration: InputDecoration(
+            hintStyle: ktextformstyle,
+            hintText: 'Table Number:',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(50.0),
+            )
+        ),
+        style: ktextstyle,
+      ),
+    );
+  }
+
+  String validateAndSetTableId(value) {
+        if ( value.isEmpty )
+          return 'Please enter the tableId';
+
+        tableId = int.tryParse( value );
+        if ( tableId == null )
+          return 'That\'s not a number dumb ass';
+
+        return null;
+      }
+}
+
+class GoToMenuButton extends StatelessWidget {
+  const GoToMenuButton({
+    Key key,
+    @required GlobalKey<FormState> formKey,
+  })  : _formKey = formKey,
+        super(key: key);
+
+  final GlobalKey<FormState> _formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        materialTapTargetSize: MaterialTapTargetSize.padded,
+        //button color
+        color: Colors.white12,
+        //The highlight color of the button's
+        highlightColor: Colors.grey,
+        //The color to use for this button's text.
+        textColor: Colors.white,
+        //We have a problem here
+        padding: EdgeInsets.all(30),
+        //The splash color of the button's
+        splashColor: Colors.deepPurple,
+        onPressed: () {
+          goToMenu( context );
+        },
+        child: Text(
+          "Go to menu",
+          style: ktextstyle,
+        ),
+      ),
+    );
+  }
+
+  goToMenu( BuildContext context ) async {
+      EasyLoading.show(status: "Checking table status..." );
+      bool requestSuccess = false;
+      await tableExists( tableId ).then( (value) => requestSuccess = value );
+      if ( !_formKey.currentState.validate() || !requestSuccess ) {
+        EasyLoading.showError( "Make sure the table exists and that it's active" );
+        return;
+      }
+
+      EasyLoading.show(status: "Fetching Menu..." );
+      List<Item> items = [];
+      List<Type> types = [];
+      await fetchItems().then( (value) => items = value );
+      await fetchTypes().then( (value) => types = value );
+      if ( items.isEmpty ) {
+        EasyLoading.showError( "Something went wrong while fetching the items. Please check with the manager" );
+        return;
+      }
+
+      EasyLoading.dismiss();
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Menu( tableId, items, types )));
+  }
+}
+
